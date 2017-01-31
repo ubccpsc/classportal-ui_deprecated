1d31536fcadbce0e3c36a9a658e2c2574e617f7f
Michael Sargent <michaelksarge@gmail.com>
2017-01-31 15:09:38 -0800

    Refactor DisplayTeam module

--- app/modules/student/DisplayTeam/DisplayTeam.js
+++ app/modules/student/DisplayTeam/DisplayTeam.js
@@ -4,9 +4,9 @@ import Module from '../../../components/Module';
 import { disbandTeamRequest } from '../../../ajax';
 import config from '../../../config';
 
-function disbandTeam(props) {
+function disbandTeam(id) {
   if (confirm('Please confirm that you want to disband the team.')) {
-    disbandTeamRequest(props.myTeamFile.id)
+    disbandTeamRequest(id)
       .then(() => {
         alert('Team has been disbanded!');
         window.location.reload(true);
@@ -16,12 +16,16 @@ function disbandTeam(props) {
 }
 
 function renderMembers(teammates) {
-  return teammates.map((val, index) => (<InputGroup.Section key={index} grow>
-    <FormIconField iconPosition="left" iconKey="mortar-board">
-      <FormInput placeholder={` ${val}`} size="sm" disabled />
-    </FormIconField>
-  </InputGroup.Section>));
+  return teammates.map((value, index) => (
+    <TeamMember key={index} name={value} />));
 }
+
+const TeamMember = (props) => (
+  <InputGroup.Section grow>
+    <FormIconField iconPosition="left" iconKey="mortar-board">
+      <FormInput placeholder={` ${props.name}`} size="sm" disabled />
+    </FormIconField>
+  </InputGroup.Section>);
 
 const RepoButton = (props) => (props.url
   ? (<InputGroup.Section>
@@ -37,24 +41,35 @@ const RepoButton = (props) => (props.url
           </Button>
   </InputGroup.Section >));
 
-RepoButton.propTypes = {
-  url: PropTypes.string,
-};
+const DisbandButton = (props) => (<InputGroup.Section>
+  <Button size="sm" onClick={() => disbandTeam(props.id)}>
+    <Glyph icon="tools" />&nbsp; Disband
+  </Button>
+</InputGroup.Section >);
 
 const DisplayTeam = (props) => (
   <Module title={`Team ${props.myTeamFile.id}`} initialHideContent={false}>
     <div>
       <InputGroup >
         {renderMembers(props.myTeamFile.members)}
-        {config.studentsCanDisbandTeams &&
-          (<InputGroup.Section>
-            <Button size="sm" onClick={disbandTeam}><Glyph icon="tools" />&nbsp; Disband</Button>
-          </InputGroup.Section>)}
+        {config.studentsCanDisbandTeams && (<DisbandButton id={props.myTeamFile.id} />)}
         <RepoButton url={props.myTeamFile.url} />
       </InputGroup>
     </div>
   </Module>
 );
+
+TeamMember.propTypes = {
+  name: PropTypes.string,
+};
+
+RepoButton.propTypes = {
+  url: PropTypes.string,
+};
+
+DisbandButton.propTypes = {
+  id: PropTypes.number,
+};
 
 DisplayTeam.propTypes = {
   myTeamFile: PropTypes.any, // eslint-disable-line
