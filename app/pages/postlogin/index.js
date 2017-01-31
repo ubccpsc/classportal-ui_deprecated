9c93b77fdbdc15945a49096ceaa557d221bc81fa
Michael Sargent <michaelksarge@gmail.com>
2017-01-31 10:47:43 -0800

    Refactor postlogin page

--- app/pages/postlogin/index.js
+++ app/pages/postlogin/index.js
@@ -3,77 +3,48 @@ import { browserHistory } from 'react-router';
 import PostLogin from '../../modules/common/PostLogin';
 import { loginRequest } from '../../../app/ajax';
 
-function getCode() {
-  return new Promise((resolve, reject) => {
-    try {
-      const url = window.location.href;
-      const validAuthCode = /[?]code=([\w\/\-]+)/; // eslint-disable-line no-useless-escape
-      if (validAuthCode.test(url)) {
-        const authcode = url.split('code=')[1];
-        resolve(authcode);
-      } else {
-        reject('Error: Invalid authcode.');
-      }
-    } catch (err) {
-      reject(err);
-    }
-  });
-}
-
 class PostloginPage extends React.Component {
   constructor() {
     super();
     this.state = { error: false };
-    this.sendCode = this.sendCode.bind(this);
-    this.handleError = this.handleError.bind(this);
   }
 
   componentDidMount() {
-    getCode()
-      .then(this.sendCode)
-      .catch(this.handleError);
-  }
-
-  /**
- * .catch() handler for postloging authentication.
- * Alerts the user with the error, then redirects to login page.
- */
-  handleError(error) {
-    alert(error);
-    localStorage.clear();
-    this.setState({ error: true }, () => {
-      // display error message for 3 seconds before redirecting to login
-      setTimeout(() => {
-        browserHistory.push('/');
-      }, 2500);
-    });
-  }
-
-
-  sendCode(authcode) {
-    loginRequest(localStorage.csid ? localStorage.csid : '', localStorage.sid ? localStorage.sid : '', authcode)
+    return Promise.resolve()
+      .then(() => {
+        const url = window.location.href;
+        const validAuthCode = /[?]code=([\w\/\-]+)/; // eslint-disable-line no-useless-escape
+        if (validAuthCode.test(url)) {
+          const authcode = url.split('code=')[1];
+          return Promise.resolve(authcode);
+        }
+        return Promise.reject('Error: Invalid authcode.');
+      })
+      .then((authcode) => loginRequest(localStorage.csid || '', localStorage.sid || '', authcode))
       .then((response) => {
-        const token = response.token;
-        const username = response.username;
-        const admin = response.admin;
-
-        if (!!username && !!token) {
+        if (!!response.username && !!response.token) {
           localStorage.clear();
-          localStorage.setItem('token', token);
-          localStorage.setItem('username', username);
-          localStorage.setItem('admin', admin);
+          localStorage.setItem('token', response.token);
+          localStorage.setItem('username', response.username);
+          localStorage.setItem('admin', response.admin);
           browserHistory.push('/');
           return Promise.resolve();
         }
-        return Promise.reject('Error loading user info.');
+        return Promise.reject('Error: There was a problem loading user info.');
       })
-      .catch(this.handleError);
+      .catch((error) => {
+        this.setState({ error: true }, () => {
+          alert(error);
+          localStorage.clear();
+          setTimeout(() => {
+            browserHistory.push('/');
+          }, 1500);
+        });
+      });
   }
 
   render() {
-    return (
-      <PostLogin error={this.state.error} />
-    );
+    return (<PostLogin error={this.state.error} />);
   }
 
 }
